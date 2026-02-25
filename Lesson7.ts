76bb96b7a0a0382058a28876475c9b1aae768a11
Sasha Sazhen <sasha.sahzen@gmail.com>
2026-02-26 00:56:22 +0100

    Fix homework 7

--- Lesson7.ts
+++ Lesson7.ts
@@ -1,29 +1,35 @@
 function filterArray<T>(arr: T[], condition: (item: T) => boolean): T[] {
-    return arr.filter(condition);
+    const result: T[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const currentItem = arr[i];
+
+        if (condition(currentItem)) {
+            result.push(currentItem)
+        }
+    }
+
+    return result;
 }
 
-const x1 = filterArray([1, 2, 3, 4, 0], (n) => n === 4);
+const x1 = filterArray<number>([1, 2, 3, 4, 0], (n) => n === 4);
 
 console.log(x1)
 
 
-type validItem<T> = {
-    [key: string]: T;
-} 
-
 class Dictionary<T>{
-    public item: validItem<T> = {};
+    public item = new Map<string, T>();
 
     setItem(key: string, value: T): void {
-        this.item[key] = value;
+        this.item.set(key, value);
     }
 
     hasItem(key: string): boolean {
-        return key in this.item;
+        return this.item.has(key);
     }
 
-    getItem(key: string): T {
-        return this.item[key]
+    getItem(key: string): T | undefined {
+        return this.item.get(key);
     }
 }
 
@@ -32,3 +38,32 @@ x2.setItem('x1', 23);
 console.log(x2)
 console.log(x2.hasItem('x1'))
 console.log(x2.getItem('x1'))
+
+class Stack<T> {
+    public tasks: T[] = [];
+
+    push(item: T): void {
+        this.tasks.push(item);
+    }
+
+    pop(): T | undefined {
+        return this.tasks.pop();
+    }
+
+    peak(): T | undefined {
+        return this.tasks[this.tasks.length - 1];
+    }
+}
+
+const b = new Stack<number>();
+
+b.push(1);
+b.push(2);
+b.push(3);
+b.push(4);
+
+console.log(b.peak());
+console.log(b.pop());
+console.log(b.peak());
+
+
