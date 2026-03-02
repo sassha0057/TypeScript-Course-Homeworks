1db689ce526aac6bb50ffd1f77ff5e16bbab56c7
Sasha Sazhen <sasha.sahzen@gmail.com>
2026-03-02 22:41:07 +0100

    Add new homework

---
+++ Lesson8.ts
@@ -0,0 +1,53 @@
+type DeepReadonly<T> = T extends object
+    ? { +readonly [K in keyof T]: DeepReadonly<T[K]>}
+    : T;
+
+interface IUser {
+    name?: string;
+    age: number;
+    courses: {
+        courseName: string;
+        grades: number[];
+    }
+}
+
+const user1: DeepReadonly<IUser> = {
+    name: 'Sasha',
+    age: 20,
+    courses: {
+        courseName: 'TypeScript',
+        grades: [1, 2, 3, 4],
+    }
+}
+
+type DeepRequireReadonly<T> = T extends object
+    ? { +readonly [K in keyof T]-?: DeepReadonly<T[K]> }
+    : T;
+
+const user2: DeepRequireReadonly<IUser> = {
+    name: 'Sasha',
+    age: 20,
+    courses: {
+        courseName: 'TypeScript',
+        grades: [1, 2, 3, 4],
+    }
+}
+
+type UpperCaseKeys<T> = { 
+    [K in keyof T as Uppercase<K & string>]: T[K];
+} 
+
+
+declare const user3: UpperCaseKeys<IUser>;
+
+
+type ObjectToPropertyDescriptor<T> = {
+    [K in keyof T]: {
+        value: T[K];
+        writable: boolean;
+        enumerable: boolean;
+        configurable: boolean;
+    }
+}
+
+declare const user4: ObjectToPropertyDescriptor<IUser>;
